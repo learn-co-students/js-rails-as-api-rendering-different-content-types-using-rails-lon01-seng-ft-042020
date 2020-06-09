9ef52c363621f541065cc1756bdd4b3b9edeacad
ewelinaszoda <ewelinaszoda@gmail.com>
2020-06-09 13:09:57 +0200

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # at rails s , localhost:3000/birds
+    # render plain: "Hello #{@birds[3].name}"
+    # render json: { message: 'Hashes of data will get converted to JSON' }
+    # render json: ['As','well','as','arrays']
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    # render json: bird
+    render json: @birds
   end
 end
